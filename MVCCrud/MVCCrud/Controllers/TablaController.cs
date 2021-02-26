3221d85391435d6c2658dbe8501fe93f2eb15496
RafaelCerva13 <cervantesromeror@gmail.com>
2021-02-25 20:48:57 -0600

    version 1

---
+++ MVCCrud/MVCCrud/Controllers/TablaController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCCrud.Models;
+using MVCCrud.Models.ViewModels;
+
+namespace MVCCrud.Controllers
+{
+    public class TablaController : Controller
+    {
+        // GET: Tabla
+        public ActionResult Index()
+        {
+            List<ListTablaViewModel> lst;
+            using (CRUDREntities db = new CRUDREntities())
+            {
+                lst = (from d in db.PERSONAL
+                           select new ListTablaViewModel
+                           {
+                               ID = d.ID,
+                               NOMBRE = d.NOMBRE,
+                               APELLIDO_P = d.APELLIDO_P,
+                               APELLIDO_M = d.APELLIDO_M,
+                               EDAD =(int) d.EDAD,
+                               IsActive = (bool) d.IsActive
+                           }).ToList();
+            }
+
+            return View(lst);
+        }
+
+
+        public ActionResult Nuevo()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(TablaViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (CRUDREntities db = new CRUDREntities())
+                    {
+                        var oTabla = new PERSONAL();
+                        oTabla.NOMBRE = model.NOMBRE;
+                        oTabla.APELLIDO_P = model.APELLIDO_P;
+                        oTabla.APELLIDO_M = model.APELLIDO_M;
+                     oTabla.EDAD = model.EDAD;
+                        oTabla.IsActive = model.IsActive;
+
+                        db.PERSONAL.Add(oTabla);
+                        db.SaveChanges();
+                    }
+
+                    return Redirect("~/Tabla/");
+                }
+
+                return View(model);
+                
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public ActionResult Editar(int ID)
+        {
+            TablaViewModel model = new TablaViewModel();
+            using (CRUDREntities db= new CRUDREntities())
+            {
+                var oTabla = db.PERSONAL.Find(ID);
+                model.NOMBRE = oTabla.NOMBRE;
+                model.APELLIDO_P = oTabla.APELLIDO_P;
+                model.APELLIDO_M = oTabla.APELLIDO_M;
+                model.EDAD =(int)oTabla.EDAD;
+                model.IsActive = (bool) oTabla.IsActive;
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(TablaViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (CRUDREntities db = new CRUDREntities())
+                    {
+                        var oTabla = db.PERSONAL.Find(model.ID);
+                        oTabla.NOMBRE = model.NOMBRE;
+                        oTabla.APELLIDO_P = model.APELLIDO_P;
+                        oTabla.APELLIDO_M = model.APELLIDO_M;
+                        oTabla.EDAD = model.EDAD;
+                        oTabla.IsActive = model.IsActive;
+
+                        db.Entry(oTabla).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                    }
+
+                    return Redirect("~/Tabla/");
+                }
+
+                return View(model);
+
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Eliminar(int ID)
+        {
+            using (CRUDREntities db = new CRUDREntities())
+            {
+              
+                var oTabla = db.PERSONAL.Find(ID);
+                db.PERSONAL.Remove(oTabla);
+                db.SaveChanges();
+            }
+            return Redirect("~/Tabla/");
+        }
+
+    }
+}
---
+++ MVCCrud/MVCCrud/Models/ViewModels/TablaViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCCrud.Models.ViewModels
+{
+    public class TablaViewModel
+    {
+
+        public int ID { get; set; }
+        [Required]
+        [StringLength(50)]
+        
+        public string NOMBRE { get; set; }
+        [Required]
+        [StringLength(15)]
+        public string APELLIDO_P { get; set; }
+        [Required]
+        [StringLength(15)]
+        public string APELLIDO_M { get; set; }
+        [Required]
+        [StringLength(15)]
+
+        public int EDAD { get; set; }
+        [Required]
+        public bool IsActive { get; set; }
+     
+
+    }
+}
